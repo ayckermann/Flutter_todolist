2dfabfd1d274be5b953565a2a3bd41d2bc9b6600
ayckermann <aya.mf12@gmail.com>
2023-10-28 19:20:51 +0700

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,239 @@
+import 'database_helper.dart';
+import 'todo.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo-List App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const TodoApp(),
+    );
+  }
+}
+
+class TodoApp extends StatefulWidget {
+  const TodoApp({Key? key}) : super(key: key);
+
+  @override
+  State<TodoApp> createState() => _TodoAppState();
+}
+
+class _TodoAppState extends State<TodoApp> {
+  final TextEditingController _searchController = TextEditingController();
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descController = TextEditingController();
+  final dbHelper = DatabaseHelper();
+  List<Todo> _todos = [];
+  // int _count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    refreshItemList();
+  }
+
+  void refreshItemList() async {
+    final todos = await dbHelper.getAllTodos();
+    setState(() {
+      _todos = todos;
+    });
+  }
+
+  void searchItems() async {
+    final keyword = _searchController.text.trim();
+    if (keyword.isNotEmpty) {
+      final todos = await dbHelper.getTodoByTitle(keyword);
+      setState(() {
+        _todos = todos;
+      });
+    } else {
+      refreshItemList();
+    }
+  }
+
+  void addItem(String title, String desc) async {
+    final todo = Todo(title: title, description: desc, completed: false);
+    await dbHelper.insertTodo(todo);
+    refreshItemList();
+  }
+
+  void updateItem(
+      Todo todo, String newTitle, String newDescription, bool completed) async {
+    final item = Todo(
+      id: todo.id,
+      title: newTitle,
+      description: newDescription,
+      completed: completed,
+    );
+    await dbHelper.updateTodo(item);
+    refreshItemList();
+  }
+
+  void deleteItem(int id) async {
+    await dbHelper.deleteTodo(id);
+    refreshItemList();
+  }
+
+  void showValueEdit(Todo todo) {
+    _titleController.text = todo.title;
+    _descController.text = todo.description;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo List'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: const InputDecoration(
+                labelText: 'Search',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (_) {
+                searchItems();
+              },
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _todos.length,
+              itemBuilder: (context, index) {
+                var todo = _todos[index];
+                return ListTile(
+                  leading: todo.completed
+                      ? IconButton(
+                          icon: const Icon(Icons.check_circle),
+                          onPressed: () {
+                            updateItem(todo, todo.title, todo.description,
+                                !todo.completed);
+                          },
+                        )
+                      : IconButton(
+                          icon: const Icon(Icons.radio_button_unchecked),
+                          onPressed: () {
+                            updateItem(todo, todo.title, todo.description,
+                                !todo.completed);
+                          },
+                        ),
+                  title: Text(todo.title),
+                  subtitle: Text(todo.description),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () {
+                      deleteItem(todo.id!);
+                    },
+                  ),
+                  onTap: () {
+                    showValueEdit(todo);
+                    showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        title: const Text('Edit Todo'),
+                        content: SizedBox(
+                          width: 200,
+                          height: 200,
+                          child: Column(
+                            children: [
+                              TextField(
+                                controller: _titleController,
+                                decoration: const InputDecoration(),
+                              ),
+                              TextField(
+                                controller: _descController,
+                                decoration: const InputDecoration(),
+                              ),
+                            ],
+                          ),
+                        ),
+                        actions: [
+                          TextButton(
+                            child: const Text('Batalkan'),
+                            onPressed: () => Navigator.pop(context),
+                          ),
+                          TextButton(
+                            child: const Text('Simpan'),
+                            onPressed: () {
+                              updateItem(
+                                todo,
+                                _titleController.text,
+                                _descController.text,
+                                todo.completed,
+                              );
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: const Text('Tambah Todo'),
+              content: SizedBox(
+                width: 200,
+                height: 200,
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _titleController,
+                      decoration: const InputDecoration(hintText: 'Judul todo'),
+                    ),
+                    TextField(
+                      controller: _descController,
+                      decoration:
+                          const InputDecoration(hintText: 'Deskripsi todo'),
+                    ),
+                  ],
+                ),
+              ),
+              actions: [
+                TextButton(
+                  child: const Text('Batalkan'),
+                  onPressed: () => Navigator.pop(context),
+                ),
+                TextButton(
+                  child: const Text('Tambah'),
+                  onPressed: () {
+                    addItem(_titleController.text, _descController.text);
+
+                    Navigator.pop(context);
+                    // setState(() {
+                    //   _count = _count + 1;
+                    // });
+                  },
+                ),
+              ],
+            ),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
